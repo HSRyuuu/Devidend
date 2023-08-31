789d10fd42228d0130e494cb0d7890d4d29a13c6
HyunSik Ryu <hsryu123@naver.com>
2023-08-31 13:51:46 +0900

    main함수에서 스크래핑 테스트

---
+++ src/main/java/com/example/devidend/DevidendApplication.java
@@ -0,0 +1,45 @@
+package com.example.devidend;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+//@SpringBootApplication
+public class DevidendApplication {
+
+    public static void main(String[] args) {
+     //   SpringApplication.run(DevidendApplication.class, args);
+
+        Connection connection = Jsoup.connect("https://finance.yahoo.com/quote/COKE/history?period1=99100800&period2=1693440000&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true");
+        try {
+            Document document = connection.get();
+
+            Elements eles = document.getElementsByAttributeValue("data-test", "historical-prices");
+            Element ele = eles.get(0);
+
+            Element tbody = ele.children().get(1);
+            for(Element e : tbody.children()){
+                String txt = e.text();
+                if(!txt.endsWith("Dividend")){
+                    continue;
+                }
+                String[] splits = txt.split(" ");
+                String month = splits[0];
+                int day = Integer.valueOf(splits[1].replace(",", ""));
+                int year = Integer.valueOf(splits[2]);
+                double dividend = Double.parseDouble(splits[3]);
+                System.out.println(year + "/" + month + "/" + day + " -> " + dividend);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/test/java/com/example/devidend/DevidendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devidend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevidendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
